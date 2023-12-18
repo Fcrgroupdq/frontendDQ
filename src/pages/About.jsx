de64a11c1e8566e408fd6a96aea85c12b9c984f7
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2023-12-18 10:49:41 +0530

    Update About.jsx
    Meta

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,16 @@ import Contact from "../components/About/Contact";
 import LetestBlog from "../components/About/LetestBlog";
 import ArticleList from "../components/About/Blog";
 import AboutTop from "../components/About/AboutTop";
+import {Helmet} from "react-helmet";
 
 const About = () => {
   return (
     <div>
+       <Helmet>
+                <meta charSet="utf-8" />
+                <title>My Title</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet>
       <AboutTop />
       <LetestBlog />
       {/* <Contact /> */}
