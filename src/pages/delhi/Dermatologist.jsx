bbeb0a4574412e6f1ec8123f21428eb0b53b6804
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-01-06 10:40:26 +0530

    Update Dermatologist.jsx
    meta

--- src/pages/delhi/Dermatologist.jsx
+++ src/pages/delhi/Dermatologist.jsx
@@ -21,15 +21,15 @@ function Dermatologist() {
       <div>
           <Helmet>
           <meta charSet="utf-8" />
-          <title>Dermatologist in Delhi | Doctors Queries</title>
+          <title>Best Dermatologist Specialist in Delhi – DoctorsQueries</title>
           <meta
               name="description"
-              content=" Become a Partner with Doctors Queries and join our network of medical professionals. Expand your network with our trusted platform. Visit at doctorsqueries.com"
+              content=" Looking for the best dermatologist specialist in Delhi? Look no further. DoctorsQueries is here to help you find the top dermatologists in the city."
           />
-          <link rel="canonical" href="https://www.doctorsqueries.com/Dermatologist-in-delhi" />
+          <link rel="canonical" href="https://www.doctorsqueries.com/delhi/dermatologist-specialist-in-delhi" />
           <meta
               name="keywords"
-              content=" Doctors Queries, Partner with Doctors Queries"
+              content=" dermatologist in Delhi, best dermatologist in Delhi, dermatologist specialist in Delhi, best dermatologist specialist in Delhi"
           />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@doctorsqueries" />
@@ -54,7 +54,7 @@ function Dermatologist() {
   <div class="container px-3 py-12 mx-auto">
       <div class="flex flex-col text-center w-full mb-6">
       <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">DoctorsQueries</h2>
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-1 text-gray-900">Best Dermatologist in Delhi</h1>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-1 text-gray-900">Best Dermatologist Specialist in Delhi</h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base">DOCTORSQUERIES is a brand new platform, our mission is to make quality healthcare affordable and accessible from anywhere.DOCTORSQUERIES is a brand new platform, our mission is to make quality healthcare affordable and accessible from anywhere.
   </p>
       </div>
