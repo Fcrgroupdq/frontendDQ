592e2e2792171d7334c55c3efc4ff9a275bdfd7b
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-01-18 15:35:58 +0530

    image

--- src/pages/noida/blog/Demo.jsx
+++ src/pages/noida/blog/Demo.jsx
@@ -66,6 +66,8 @@ function Demo() {
             </div>
 
             <div class="conatiner-fluid">
+            <img class="w-full my-4" src="https://images.pexels.com/photos/5120892/pexels-photo-5120892.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=625.0&sharp=10&w=1500" alt="Sunset in the mountains">
+</a> 
               <p>
                 
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non
@@ -185,9 +187,7 @@ function Demo() {
           </div>
         </section>
       </div>
-      {doctors.map((item) => (
-        <DoctorCart data={item} />
-      ))}
+    
 
       {/* Quick Links */}
       {/* <section class="text-gray-600 body-font">
