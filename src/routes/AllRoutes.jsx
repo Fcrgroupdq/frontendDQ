80ceeed9905ef882281539472bee9b923a6a360b
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-01-18 17:56:37 +0530

    Route

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -199,7 +199,7 @@ const Allroute = () => {
       <Route path="/about" element={<About />} />
 
       {/* blog page route */}
-      <Route path="/dehi/blog" element={<delhiDemo />} />
+      <Route path="blog/where-to-find-the-best-general-physician-in-delhi" element={<generalphyD />} />
       <Route path="/gurgaon/blog" element={<gurgaonDemo />} />
       <Route path="blog/where-to-find-the-best-general-physician-in-delhi" element={< Demo/>} />
       {/* <Route path="/de" element={<About2 />} /> */}
