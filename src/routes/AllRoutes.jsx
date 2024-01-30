da52909970bca311df73aad052b97c7f79504069
datadump7259 <datadump7259@gmail.com>
2024-01-30 11:18:47 +0530

    copm

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -205,7 +205,7 @@ const Allroute = () => {
       {/* <Route path="blog/where-to-find-the-best-general-physician-in-delhi" element={< Generalphy />} />
       <Route path="/gurgaon/blog" element={<gurgaonDemo />} /> */}
       <Route path="blog/where-to-find-the-best-general-physician-in-delhi" element={< Generalphy/>} />
-      <Route path="blog/dentist-in-delhi" element={< dentistdelhi/>} />
+      <Route path="blog/dentist-in-delhi2" element={< dentistdelhi/>} />
       {/* <Route path="/de" element={<About2 />} /> */}
 
       <Route path="/doctorappointment" element={<DoctorAppointment />} />
