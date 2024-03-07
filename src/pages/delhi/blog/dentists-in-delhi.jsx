1b79e33f8f0121c361e3f5673da8c206208009a1
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-07 15:37:43 +0530

    image added

--- src/pages/delhi/blog/dentists-in-delhi.jsx
+++ src/pages/delhi/blog/dentists-in-delhi.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import DoctorCart from "../../../components/doctors/doctorCart";
 import axios from "axios";
 import { Helmet } from "react-helmet";
-
+import dq from "../../assets/blog/delhi/dentist.webp"
 function Dentistsindelhi() {
   const [doctors, setDoctors] = useState([]);
 
@@ -57,6 +57,7 @@ function Dentistsindelhi() {
             <div class="conatiner-fluid">
               <img class="w-full my-4" src="https://images.pexels.com/photos/5120892/pexels-photo-5120892.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=625.0&sharp=10&w=1500" alt="Sunset in the mountains" />
 
+              <img class="w-full my-4" src="{dentist}" alt="Sunset in the mountains" />
               <p>
               Dentists in Delhi&nbsp;offer world-class dental services at a fraction of the cost without compromising on quality, making it an ideal choice for dental tourists. Delhi boasts state-of-the-art <a href="https://www.doctorsqueries.com/doctors"><strong><em><u>dental facilities</u></em></strong></a>&nbsp;that adhere to international standards of hygiene and safety.<br /> <br />
               Delhi has become famous for Cosmetic dentistry and smile makeovers. Cosmetic dentistry allows you to talk, smile, laugh, and eat in front of others without worrying about having your teeth judged. As you become more outgoing, people will be drawn to your positive attitude. This encourages you to socialize more, further boosting your self-confidence.
