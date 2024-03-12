708d426591982997013608bf0fa431b3b9a3d7b0
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-12 17:56:22 +0530

    Logo Edit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -69,11 +69,13 @@ export default function Navbar() {
                 </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex flex-shrink-0 items-center">
+                    <a href="https://www.doctorsqueries.com">
                     <img
                       className="h-11 w-auto"
                       src={dq}
-                      alt="Your Company"
+                      alt="Doctorsqueries"
                     />
+                    </a>
                   </div>
                   <div className="hidden sm:ml-6 sm:block">
                     <div className="flex space-x-4">
