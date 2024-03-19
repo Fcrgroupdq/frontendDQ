f472b27fa91f7c9329b1603e499b06df07df9ece
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-19 12:13:35 +0530

    Href

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -55,7 +55,7 @@ const Blog = () => {
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
             <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
             <div class="flex items-center flex-wrap">
-              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" href='https://www.doctorsqueries.com' >Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
