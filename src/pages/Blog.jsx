db632725b88c37da436cfb9cf5eaf2beacad7dec
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-26 11:54:54 +0530

    3rd Card

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 // import BlogModel from '../components/Blog/BlogModel'
 // import LetestBlog from '../components/About/LetestBlog'
 import dentist from "../assets/blog/delhi/dentist.webp"
+import hospitaldent from "../assets/blog/delhi/dental-treatment-in-delhi"
 import hospitaltop5 from "../assets/blog/delhi/list-of-top-5-best-hospital-in-delhi.webp"
 import wheregenphy from "../assets/blog/delhi/where_to_find_the_best_general_physician_in_delhi.webp"
 
@@ -30,6 +31,27 @@ const Blog = () => {
  <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap -m-4">
+
+    <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+        <img class="w-full my-0 object-fill" src={hospitaldent} alt="List of Top 5 Best Hospital in Delhi- Find the Best" />
+          <div class="p-6">
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">DoctorsQueries</h2>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Top 5 Best Hospitals in Delhi - Updated List 2024</h1>
+            <p class="leading-relaxed mb-3">Find the best hospital in Delhi with the list of top 5 hospitals on DoctorsQueries. Get the best medical care and treatment for your needs.</p>
+            <div class="flex items-center flex-wrap ">
+              <a href='https://www.doctorsqueries.com/list-of-top-5-best-hospital-in-delhi' class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Read More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
 
 
     <div class="p-4 md:w-1/3">
--- src/pages/delhi/blog/best_hospital_for_dental_treatment.jsx
+++ src/pages/delhi/blog/best_hospital_for_dental_treatment.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import DoctorCart from "../../../components/doctors/doctorCart";
 import axios from "axios";
 import { Helmet } from "react-helmet";
+import hospitaldent from "../../../assets/blog/delhi/dental-treatment-in-delhi.webp"
+
+
 
 function DentaltreatmentD() {
   const [doctors, setDoctors] = useState([]);
@@ -48,14 +51,14 @@ function DentaltreatmentD() {
                 DoctorsQueries
               </h2>
              
-              <h1 class="sm:text-5xl font-medium text-gray-900">Which is the best hospital for dental treatment in Delhi?</h1>
+              <h1 class="sm:text-5xl text-4xl font-medium text-gray-900">Which is the best hospital for dental treatment in Delhi?</h1>
 
                 
               
             </div>
 
             <div class="conatiner-fluid">
-              {/* <img class="w-full my-4" src="https://images.pexels.com/photos/5120892/pexels-photo-5120892.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=625.0&sharp=10&w=1500" alt="Sunset in the mountains" /> */}
+              <img class="w-full my-4" src={hospitaldent} alt="Sunset in the mountains" />
 
               <p>
               The importance of <b><i>dental treatment in Delhi</i></b> lies in its comprehensive and specialized approach, advanced facilities, preventive focus, and the commitment of experienced professionals to promote optimal oral health and well-being.
@@ -110,7 +113,7 @@ function DentaltreatmentD() {
                 </li>
               </ul>
             
-              <h3 class="sm:text-4xl font-medium text-gray-900 mt-6 mb-2">
+              <h3 class="sm:text-4xl text-3xl font-medium text-gray-900 mt-6 mb-2">
               Some of the best Facilities of Dental Treatment in Delhi are:
               </h3>
              
