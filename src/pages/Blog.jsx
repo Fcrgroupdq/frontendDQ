158a2f49ee0519ed1354ac57924f369e2db61dab
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-28 11:30:49 +0530

    Card Done

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -32,15 +32,39 @@ const Blog = () => {
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap -m-4">
 
+
     <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <img class="w-full my-0 object-fill" src={hospitaldent} alt="List of Top 5 Best Hospital in Delhi- Find the Best" />
           <div class="p-6">
             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">DoctorsQueries</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Top 5 Best Hospitals in Delhi - Updated List 2024</h1>
-            <p class="leading-relaxed mb-3">Find the best hospital in Delhi with the list of top 5 hospitals on DoctorsQueries. Get the best medical care and treatment for your needs.</p>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Which is the best hospital for dental treatment in Delhi?</h1>
+            <p class="leading-relaxed mb-3">The importance of dental treatment in Delhi lies in its comprehensive and specialized approach, advanced facilities, preventive focus, and the commitment of experienced professionals to promote optimal oral health and well-being.</p>
             <div class="flex items-center flex-wrap ">
-              <a href='https://www.doctorsqueries.com/list-of-top-5-best-hospital-in-delhi' class="text-indigo-500 hover:text-red inline-flex items-center md:mb-2 lg:mb-0">Read More
+              <a href='https://www.doctorsqueries.com/dental-treatment-in-delhi' class="text-indigo-500 hover:text-red inline-flex items-center md:mb-2 lg:mb-0">Read More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+    <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+        <img class="w-full my-0 object-fill" src={hospitaldent} alt="List of Top 5 Best Hospital in Delhi- Find the Best" />
+          <div class="p-6">
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">DoctorsQueries</h2>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Which is the best hospital for dental treatment in Delhi?</h1>
+            <p class="leading-relaxed mb-3">The importance of dental treatment in Delhi lies in its comprehensive and specialized approach, advanced facilities, preventive focus, and the commitment of experienced professionals to promote optimal oral health and well-being.</p>
+            <div class="flex items-center flex-wrap ">
+              <a href='https://www.doctorsqueries.com/dental-treatment-in-delhi' class="text-indigo-500 hover:text-red inline-flex items-center md:mb-2 lg:mb-0">Read More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
