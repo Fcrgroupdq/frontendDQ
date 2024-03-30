789252c0ee90b3384c896f4fd8c352e66e3b1e00
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-30 16:24:22 +0530

    7th Card

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import BlogCart from '../components/Blog/BlogCart'
 // import BlogModel from '../components/Blog/BlogModel'
 // import LetestBlog from '../components/About/LetestBlog'
+import gurgaondentist from "../assets/blog/gurgaon/best-hospital-for-dental-treatment-in-gurgaon.jpg"
 import cosmeticsouth from "../assets/blog/delhi/cosmetic-surgeons-in-south-delhi.jpg"
 import govhospital from "../assets/blog/delhi/govt_hospital_in_delhi.jpg"
 import dentist from "../assets/blog/delhi/dentist.webp"
@@ -33,6 +34,29 @@ const Blog = () => {
  <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap -m-4">
+
+
+    <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+        <img class="w-full my-0 object-fill" src={gurgaondentist} alt="Which is the best hospital for dental treatment in Gurgaon?" />
+          <div class="p-6">
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">DoctorsQueries</h2>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Which is the best hospital for dental treatment in Gurgaon?</h1>
+            <p class="leading-relaxed mb-3">When it comes to finding the best hospital for dental treatment in Gurgaon, can be troublesome. With so many options to choose from, how do you know which hospital will provide the highest quality care and the best outcomes for your specific needs? </p>
+            <div class="flex items-center flex-wrap ">
+           <button>
+              <a href='https://www.doctorsqueries.com/best-hospital-for-dental-treatment-in-gurgaon' class="text-indigo-500 hover:text-red inline-flex items-center md:mb-2 lg:mb-0">Read More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
     <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
--- src/pages/gurgaon/blog/dental_treatment_in_gurgaon.jsx
+++ src/pages/gurgaon/blog/dental_treatment_in_gurgaon.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import DoctorCart from "../../../components/doctors/doctorCart";
 import axios from "axios";
 import { Helmet } from "react-helmet";
-
+import gurgaondentist from "../../../assets/blog/delhi/best-hospital-for-dental-treatment-in-gurgaon.jpg"
 
 function Dentaltreatment() {
   const [doctors, setDoctors] = useState([]);
@@ -56,7 +56,7 @@ function Dentaltreatment() {
             </div>
 
             <div class="conatiner-fluid">
-              {/* <img class="w-full my-4" src="https://images.pexels.com/photos/5120892/pexels-photo-5120892.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=625.0&sharp=10&w=1500" alt="Sunset in the mountains" /> */}
+              <img class="w-full my-4" src={gurgaondentist} alt="Which is the best hospital for dental treatment in Gurgaon?" />
 
               <p>
               When it comes to finding the     <b>
